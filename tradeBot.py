64b1bfa2999c67f7be17cce4ffa26e98999c5d85
kani-tech <kanishksk@gmail.com>
2021-11-17 20:21:23 -0500

    start building momentum algorithm

--- tradeBot.py
+++ tradeBot.py
@@ -14,8 +14,21 @@ pos_held = False
 while True:
     print("")
     print("Checking Price")
-    market_data = api.get_barset(symb, 'minute', limit=5)
-    close_list = []
+    market_data = api.get_barset([symb], 'day', limit=31)
+    print(market_data)
+    backtrack_time = 20
+
+    # Find the closing price for the past 30 dalys
+    closing_data = []
+    for bar in market_data[symb]:
+        closing_data.append(bar.c)
+
+    vol_today = np.std(closing_data[1:31])
+    vol_yesterday = np.std(closing_data[1:30])
+    change_in_vol = (vol_today - vol_yesterday) / vol_today
+    backtrack_time = round(backtrack_time * (1 + change_in_vol))
+
+    """ close_list = []
     for bar in market_data[symb]:
         close_list.append(bar.c)
 
@@ -48,6 +61,6 @@ while True:
             type="market",
             time_in_force='gtc'
         )
-        pos_held = False
+        pos_held = False """
 
     time.sleep(60)
